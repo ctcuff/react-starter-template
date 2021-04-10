92f83d7f2aa728340bf75cbbc1b22aa51fc94930
ctcuff <dev.ctcuff@gmail.com>
2021-04-10 01:28:23 -0400

    Project can now be viewed on network

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ESLintPlugin = require('eslint-webpack-plugin')
 const { HotModuleReplacementPlugin } = require('webpack')
 
+/** @type {import('webpack').Configuration} */
 module.exports = {
   plugins: [
     new ESLintPlugin(),
-    new HotModuleReplacementPlugin(),
+    new HotModuleReplacementPlugin({
+      logLevel: 'warning'
+    }),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html'),
       title: 'React App'
@@ -22,7 +25,11 @@ module.exports = {
     contentBase: path.resolve(__dirname, 'public'),
     open: true,
     port: 9000,
-    hot: true
+    hot: true,
+    useLocalIp: true,
+    // Using 0.0.0.0 allows the project to be accessed by all
+    // devices on the same network as the host
+    host: '0.0.0.0'
   },
   module: {
     rules: [
@@ -37,12 +44,18 @@ module.exports = {
         use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
       },
       {
-        test: /\.(png|svg)/,
+        // Using asset/resource will emit a separate file and export the
+        // url. If you want svg to be imported inline, separate svg into a
+        // new rule and use asset/inline for (data URI) or asset/source (for code).
+        test: /\.(png|jpg|svg)/,
         type: 'asset/resource'
       }
     ]
   },
   resolve: {
+    // Files without an extension will be treated as one of these.
+    // Note that webpack will go from left to right until the proper
+    // extension is found.
     extensions: ['.js', '.jsx']
   }
 }
