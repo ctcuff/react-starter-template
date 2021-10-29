5ed07c95d28979df794a300d51de1021e62d1d2e
ctcuff <dev.ctcuff@gmail.com>
2021-10-28 20:20:31 -0400

    Replace node-sass with sass

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html'),
+      favicon: path.resolve(__dirname, 'public', 'favicon.ico'),
       title: 'React App'
     })
   ],
