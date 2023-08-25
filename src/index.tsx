fd2aede25f8aed4b56f227bffb74fe615d6160dd
ctcuff <dev.ctcuff@gmail.com>
2023-08-25 15:48:33 -0700

    Update dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,5 @@ if (module.hot) {
   module.hot.accept()
 }
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(document.getElementById('app')!)
 root.render(<App />)
