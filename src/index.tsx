3bda6088113a02c21ff0f02845c8423870b4d43d
ctcuff <dev.ctcuff@gmail.com>
2023-08-25 01:49:45 -0700

    Dependency updates
    - Upgrade to React 18
    - Remove Airbnb's eslint config due to compilation errors
    
    Update to React 18
    
    Update dependencies
    
    Update dependencies
    
    Update dependencies
    
    Update dependencies
    
    Update dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import './style/index.scss'
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import App from './components/App'
 
 if (module.hot) {
   module.hot.accept()
 }
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const root = createRoot(document.getElementById('app')!)
+root.render(<App />)
